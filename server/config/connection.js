7744cb9cda05df9a63541b1fef9be4bc1167d5b0
Jeanette Ralph <jean.ralph@hotmail.com>
2022-09-28 16:56:24 -0700

    added code for jwt

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,11 +1,10 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(
-    process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/tech-friends',
-    {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    }
-);
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/gloomhaven-player-helper-app', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+});
 
 module.exports = mongoose.connection;
