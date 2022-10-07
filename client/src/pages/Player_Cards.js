02013c246138335224bb6bfbb5439b5600122d13
OddVidaRoad <oddvidaroad@gmail.com>
2022-10-07 15:02:12 -0700

    Stuff

--- client/src/pages/Player_Cards.js
+++ client/src/pages/Player_Cards.js
@@ -4,49 +4,64 @@ import DeckMaker from "../components/DeckMaker";
 import { useQuery } from "@apollo/client";
 import { QUERY_CARDS } from "../utils/queries";
 
-const SavedPlayerCards = () => {
-  const [userData, setUserData] = useState({});
+  //const SavedPlayerCards = () => {
+  //const [userData, setUserData] = useState({});
 
-  const userDataLength = Object.keys(userData).length;
+  //const userDataLength = Object.keys(userData).length;
 
-  useEffect(() => {
-    const getUserData = async () => {
-      try {
-        const token = Auth.loggedIn() ? Auth.getToken() : null;
+  const Cards = (props) => {
+    const { loading, data } = useQuery(QUERY_CARDS);
+    console.log("--------------playercards-----------", props);
+    const cards = data?.cards || [];
+    return (
+      <div>
+        {loading ? (
+          <div>Loading...</div>
+        ) : (
+          <DeckMaker cards={cards}>{props.children}</DeckMaker>
+        )}
+      </div>
+    );
 
-        if (!token) {
-          return false;
-        }
+//   useEffect(() => {
+//     const getUserData = async () => {
+//       try {
+//         const token = Auth.loggedIn() ? Auth.getToken() : null;
 
-        const response = await QUERY_CARDS(token);
+//         if (!token) {
+//           return false;
+//         }
 
-        if (!response.ok) {
-          throw new Error("something went wrong!");
-        }
+//         const response = await QUERY_CARDS(token);
 
-        const user = await response.json();
-        setUserData(user);
-      } catch (err) {
-        console.error(err);
-      }
-    };
+//         if (!response.ok) {
+//           throw new Error("something went wrong!");
+//         }
 
-    getUserData();
-  }, [userDataLength]);
+//         const user = await response.json();
+//         setUserData(user);
+
+//       } catch (err) {
+//         console.error(err);
+//       }
+//     };
+
+//     getUserData();
+//   }, [userDataLength]);
+// };
+
+// const Cards = (props) => {
+//   const { loading, data } = useQuery(QUERY_CARDS);
+//   console.log("--------------playercards-----------", props);
+//   const cards = data?.cards || [];
+//   return (
+//     <div>
+//       {loading ? (
+//         <div>Loading...</div>
+//       ) : (
+//         <DeckMaker cards={cards}>{props.children}</DeckMaker>
+//       )}
+//     </div>
+//   );
 };
-
-const Cards = (props) => {
-  const { loading, data } = useQuery(QUERY_CARDS);
-  console.log("--------------playercards-----------", props);
-  const cards = data?.cards || [];
-  return (
-    <div>
-      {loading ? (
-        <div>Loading...</div>
-      ) : (
-        <DeckMaker cards={cards}>{props.children}</DeckMaker>
-      )}
-    </div>
-  );
-};
-export default SavedPlayerCards;
+export default Cards;
