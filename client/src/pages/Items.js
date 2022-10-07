5bdee81809b378a95cc0d981bf92bbd90c45d132
Jeanette Ralph <jean.ralph@hotmail.com>
2022-10-06 20:06:57 -0700

    adding auth to pages

--- client/src/pages/Items.js
+++ client/src/pages/Items.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import ItemMaker from "../components/ItemMaker";
 import { useQuery } from "@apollo/client";
