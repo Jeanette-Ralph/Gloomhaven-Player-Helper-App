e5c878a1d6c7bd53e313f1360455fdc1c961b3e0
James Plasencia <jamesplasencia@gmail.com>
2022-10-07 21:55:08 -0700

    last push

--- client/src/pages/Play_Mat.js
+++ client/src/pages/Play_Mat.js
@@ -1,11 +1,13 @@
 import React from "react";
 import ItemSummary from "../components/ItemSummary";
 import CardSummary from "../components/CardSummary";
+import StatCounter from "../components/StatCounter";
 
 const Play_Mat = (props) => {
   console.log("----playmat------", props.cards);
   return (
     <div>
+      <StatCounter />
       <CardSummary cards={props.cards} deck={props.deck} />
       <ItemSummary items={props.items} item={props.items} />
     </div>
